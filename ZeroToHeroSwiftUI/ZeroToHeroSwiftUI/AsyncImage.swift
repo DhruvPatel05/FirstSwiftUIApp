aba8965e30a44a5adcb6ac6c64cbdcc2bc13b081
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-22 16:19:14 -0700

    Image loading with different animation added

--- ZeroToHeroSwiftUI/ZeroToHeroSwiftUI/AsyncImage.swift
+++ ZeroToHeroSwiftUI/ZeroToHeroSwiftUI/AsyncImage.swift
@@ -32,16 +32,47 @@ struct AsyncImageView: View {
         // MARK: - 2 SCALE
 //        AsyncImage(url: URL.init(string: imageUrl), scale: 3.0)
         
-        // MARK: - 3 PLACE HOLDER
-        AsyncImage(url: URL(string: imageUrl)) { image in
-            image.ImageModifier()
-        }
-            placeholder : {
-                  Image(systemName: "photo.circle.fill")
-                    .IconModifier()
-                }
-            .padding(40)
+//        // MARK: - 3 PLACE HOLDER
+//        AsyncImage(url: URL(string: imageUrl)) { image in
+//            image.ImageModifier()
+//        }
+//            placeholder : {
+//                  Image(systemName: "photo.circle.fill")
+//                    .IconModifier()
+//                }
+//            .padding(40)
+        // MARK: - 4 PHASE
+//        AsyncImage.init(url: URL.init(string: imageUrl)) { phase in
+//            if let image = phase.image {
+//                image.ImageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill").IconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill").IconModifier()
+//            }
+//        }
+//            .padding(40)
+        // MARK: - 5 Animation
+        AsyncImage(url: URL.init(string: imageUrl), transaction: Transaction(animation:  .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) {
+            phase in
+            switch phase {
+            case .success(let image):
+                image.ImageModifier()
+//                    .transition(.move(edge: .bottom))
+//                    .transition(.slide)
+                    .transition(.scale)
+                
+            case .empty:
+                Image.init(systemName: "photo.circle.fill").IconModifier()
             
+            case .failure(_):
+                Image.init(systemName: "ant.circle.fill").IconModifier()
+            @unknown default:
+                ProgressView()
+            }
+            
+        }
+        .padding(40)
         }
     }
 #Preview {
